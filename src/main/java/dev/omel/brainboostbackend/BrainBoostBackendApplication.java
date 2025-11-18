072b13b2a724076e25f017bfc2a9ee48ae126e66
Omel (Leon) <michelson45@gmail.com>
2025-11-18 09:22:15 +0100

    Ini Commit

---
+++ src/main/java/dev/omel/brainboostbackend/BrainBoostBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.omel.brainboostbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrainBoostBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrainBoostBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/omel/brainboostbackend/BrainBoostBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.omel.brainboostbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrainBoostBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
