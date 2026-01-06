5a1becc5d80f3f697be64418ebd2d009c83bb5fe
Omel (Leon) <michelson45@gmail.com>
2026-01-06 11:14:48 +0100

    Add custom query to UserRepository for username existence check

--- src/main/java/dev/omel/brainboostbackend/repository/UserRepository.java
+++ src/main/java/dev/omel/brainboostbackend/repository/UserRepository.java
@@ -2,6 +2,7 @@ package dev.omel.brainboostbackend.repository;
 
 import dev.omel.brainboostbackend.domain.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,5 +11,6 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
     Optional<UserEntity> findByUsernameAndPasswordHash(String username, String passwordHash);
 
+    @Query("SELECT CASE WHEN COUNT(u) > 0 THEN true ELSE false END FROM UserEntity u WHERE u.username = ?1")
     boolean findByUsername(String username);
 }
