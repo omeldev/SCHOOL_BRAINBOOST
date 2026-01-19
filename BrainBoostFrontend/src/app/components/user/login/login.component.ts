d1990445410c2776f8bc31c81dafc71287c116e8
Omel (Leon) <michelson45@gmail.com>
2026-01-19 10:45:52 +0100

    Refactor LoginComponent to use reactive forms for improved state management

--- BrainBoostFrontend/src/app/components/user/login/login.component.ts
+++ BrainBoostFrontend/src/app/components/user/login/login.component.ts
@@ -1,52 +1,48 @@
-import {Component} from '@angular/core';
+import {Component, signal} from '@angular/core';
 import {Store} from '@ngxs/store';
 import {UserService} from '../../../service/rest/user/user.service';
-import {BehaviorSubject, combineLatest, firstValueFrom, map} from 'rxjs';
+import {firstValueFrom} from 'rxjs';
 import {LoginBean} from '../../../bean/user';
 import {UserAction} from '../../../store/user/user.actions';
 import {Router, RouterLink} from '@angular/router';
 import {ToastAction} from '../../../store/toast/toast.action';
 import {ToastType} from '../../../bean/ToastBean';
+import {form, FormField} from '@angular/forms/signals';
+
+interface LoginFormData {
+  username: string;
+  password: string;
+}
 
 @Component({
   selector: 'app-login',
   imports: [
-    RouterLink
+    RouterLink,
+    FormField
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
 export class LoginComponent {
 
-  private readonly userNameSubject$ = new BehaviorSubject("");
-  public readonly userName$ = this.userNameSubject$.asObservable();
+  private readonly loginFormModel = signal<LoginFormData>({
+    username: '',
+    password: ''
+  });
 
-  private readonly passwordSubject$ = new BehaviorSubject("");
-  public readonly password$ = this.passwordSubject$.asObservable();
-
-
-  public readonly user$ = combineLatest([this.userName$, this.password$]).pipe(
-    map(([userName, password]): LoginBean => ({
-      username: userName,
-      password: password
-      })
-    ));
+  public readonly loginForm = form(this.loginFormModel);
 
   constructor(private readonly store: Store,
               private readonly userService: UserService,
               private readonly router: Router) {
   }
 
-  public nextUserName(event: Event) {
-    this.userNameSubject$.next((event.target as HTMLInputElement).value);
-  }
-
-  public nextPassword(event: Event) {
-    this.passwordSubject$.next((event.target as HTMLInputElement).value);
-  }
 
   public async login$() {
-    const user = await firstValueFrom(this.user$);
+    const user: LoginBean = {
+      username: this.loginFormModel().username,
+      password: this.loginFormModel().password
+    }
 
     if(user.username.trim() === '') {
       this.store.dispatch(new ToastAction.ShowToast({
